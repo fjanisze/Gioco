e1eded96f41735a17afddaa97363a712d6f141ac
fjanisze <filip.7an@gmail.com>
2013-03-02 12:12:01 +0100

    Start to work on the goods tool from the beginning

--- economic/goods.cpp
+++ economic/goods.cpp
@@ -2,6 +2,27 @@
 
 namespace goods_manager
 {
+	good_node_t::good_node_t() : child_node( nullptr ), parent_node( nullptr ), leaf_node( nullptr ),
+		next( nullptr ), prev( nullptr )
+	{
+	}
 
+	//Create and empty node
+	good_node_t* good_container::create_node()
+	try{
+		good_node_t* node = new good_node_t;
+		return node;
+	}catch( std::exception& xa )
+	{
+		LOG_ERR("good_container::create_node(): Exception catched, what: ",xa.what() );
+		return nullptr;
+	}
+
+	//Contructor
+	good_container::good_container() : root( nullptr )
+	{
+		//Create an empty container;
+		root = create_node();
+	}
 }
 
--- economic/goods.hpp
+++ economic/goods.hpp
@@ -16,6 +16,34 @@ namespace goods_manager
 	using namespace finance;
 	using namespace std;
 
+	//The leaf node will contain the data itself
+	struct leaf_node_t
+	{
+	};
+
+	//Node of the tree structure
+	struct good_node_t
+	{
+		//Parental hierarchies
+		good_node_t* child_nodes,
+				* parent_node;
+		//Link to the nodes at the same level
+		good_node_t* next,
+			*prev;
+		//Eventually, link to the leaf node
+		leaf_node_t* leaf_node; 
+
+		good_node_t():
+	};
+
+	//Good container
+	class good_container
+	{
+		good_node_t* root; //of the whole structure structure
+		good_node_t* create_node();
+	public:
+		good_container();
+	};
 }
 
 

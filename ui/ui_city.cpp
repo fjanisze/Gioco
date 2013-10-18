b6a9a51d79cf98a8eac97cd158ae15c02510924d
fjanisze <filip.7an@gmail.com>
2013-10-18 11:54:18 +0200

    Small fix in handle_new_construction

--- ui/ui_city.cpp
+++ ui/ui_city.cpp
@@ -205,7 +205,7 @@ namespace city_ui_manager
     //This function is called when the user has chosen which building want to build and has clicked on the map for the place
     bool city_ui::handle_new_construction()
     {
-        ELOG("city_ui::handle_new_construction(): The user want to build a new building, building ID:",build_info.building_id,", field ID:", build_info.field->field_id,",city ID:",build_info.field->descriptor->obj_id );
+        ELOG("city_ui::handle_new_construction(): The user want to build a new building, building ID:",build_info.building_id,", field ID:", build_info.field->field_id,",city ID:",city_agent->get_city_id());
         //Get the construction manager and try to build the building
         bool verdict = game_manager::game_manager::get_instance()->user_want_start_construction( build_info.building_id , city_agent->get_city_id() , build_info.field->field_id );
         return verdict;
